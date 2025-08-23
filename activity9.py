4b65a79a624d17747100de5c1a2d932f4a6677dc
sandara318 <belwan318@gmail.com>
2025-08-23 21:10:19 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,15 @@
+a = 5
+b = 3
+
+c = 8
+d = 7
+
+username = 'Belwan'
+password = 'labyou'
+
+print("USER LOGIN ")
+print( (username == 'Belwan') and (password == 'labyou'))
+
+#print( (a > b) and (c < d) )
+#print( (a < b) or (c > d) )
+print( not((a < b) or (c < d)) )
