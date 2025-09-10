5c4d17d621947addd8bf17997300717250913636
sandara318 <belwan318@gmail.com>
2025-09-10 22:14:53 +0800

    Add files via upload

--- activity11.py
+++ activity11.py
@@ -1,20 +1,7 @@
-temp = eval(input("Enter Temperature outside --> "))
+Temp = eval(input("Enter Temperature outside --> "))
 
-#input 40
-if temp >=1 and temp <= 20:
-	print("Temperature is Considered as Extremly Cold")
-if temp >=21 and temp <= 31:
-	print("Temperature is Considered as Moderately Cold")
-if temp >=34 and temp <= 37:
-	print("Temperature is Considered as Lukewarm")
-if temp >=38 and temp <= 45:
-	print("Temperature is Considered as Hot")
-if temp >=46 and temp <= 54:
-	print("Temperature is Considered as Boiling Hot")
-if temp >=55 and temp <= 62:
-	print("Temperature is Considered as Dangerous Temperature")
-
+#input 45
+if temp >= 0 and <= 0:
+	print("Temperature is considered as Freezing Temperature")
 else:
-	print("Invalid Temperature")
-
-
+	print("Invalid Temperature") 
